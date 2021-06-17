1e272ce5c52fa191d8d2377a008e582df6fdaad7
Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
2021-06-17 14:41:07 -0400

    following objects position update script

--- Nanovision-AR/Assets/Scripts/FollowPlayer.cs
+++ Nanovision-AR/Assets/Scripts/FollowPlayer.cs
@@ -10,25 +10,32 @@ public class FollowPlayer : MonoBehaviour
 {
 	[SerializeField] List<GameObject> followingObjects;
 	[SerializeField] float circleRadius;
-	// Start is called before the first frame update
+
+	private List<Vector3> outvectors;
+	private int numObjects;
+	private Vector3 cameraPosition;
 	void Start()
 	{
-		int numObjects = followingObjects.Count;
-		Vector3 cameraPosition = Camera.main.transform.position;
+		numObjects = followingObjects.Count;
+		cameraPosition = Camera.main.transform.position;
 		float angle = 360f / (float)numObjects;
 		for (int i = 0; i < numObjects; i++)
 		{
 			Quaternion rotation = Quaternion.AngleAxis(i * angle, Vector3.up);
-			Vector3 direction = rotation * Vector3.forward;
-			Vector3 position = cameraPosition + (direction * circleRadius);
+			Vector3 startOrientation = Vector3.forward;
+			Vector3 direction = rotation * startOrientation;
+			outvectors[i] = direction * circleRadius;
+			Vector3 position = cameraPosition + outvectors[i];
 			Instantiate(followingObjects[i], position, rotation);
-
 		}
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-
+		cameraPosition = Camera.main.transform.position;
+		for (int i = 0; i < numObjects; i++)
+		{
+			followingObjects[i].transform.position = cameraPosition + outvectors[i];
+		}
 	}
 }
