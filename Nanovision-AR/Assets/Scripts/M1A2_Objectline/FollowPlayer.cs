7060ec83f31d2f221f388e31468625184a3620c4
Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
2021-07-05 13:38:37 -0400

    Squashed commit of the following:
    commit 88155d5d3e7409e5e52a0f10f2ed1adfa13c2fcb
    Author: Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
    Date:   Sun Jul 4 22:18:50 2021 -0400
    
        corrected labels, scaling fixed, folder organized
    
    commit 0c8e36fff4e7f6c6634c17d5e95b7c9e24d5b729
    Author: Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
    Date:   Thu Jul 1 11:23:50 2021 -0400
    
        Update FollowPlayer.cs
    
    commit e1cd0d90918328e2a59a2e8b792126d01426bcc8
    Author: Rashi Tyagi <56127515+rashicode99@users.noreply.github.com>
    Date:   Mon Jun 28 20:10:36 2021 -0400
    
        labels and new models
    
    commit e45bfd253ce0e5a93d5a339df6cc3b29c8c197a7
    Author: Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
    Date:   Mon Jun 21 21:52:08 2021 -0400
    
        objects placed in a line instead of ring
    
    commit 97c4885c96303d48ca53fd29110c9e7287943345
    Author: Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
    Date:   Fri Jun 18 12:50:14 2021 -0400
    
        created materials for models
    
    commit 5e8e99fd26206c4e4f96a412b5cb48a66a1d8452
    Author: Rashi Tyagi <56127515+rashicode99@users.noreply.github.com>
    Date:   Fri Jun 18 12:05:58 2021 -0400
    
        added models
    
    commit 1e272ce5c52fa191d8d2377a008e582df6fdaad7
    Author: Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
    Date:   Thu Jun 17 14:41:07 2021 -0400
    
        following objects position update script
    
    commit 28ef375a38d4070a707f8867f6ef4b923c2eca6f
    Author: Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
    Date:   Thu Jun 17 00:14:35 2021 -0400
    
        instantiate objects around players

---
+++ Nanovision-AR/Assets/Scripts/M1A2_Objectline/FollowPlayer.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System;
+using UnityEngine.XR.ARSubsystems;
+
+public class FollowPlayer : MonoBehaviour
+{
+        
+	[SerializeField] List<GameObject> followingObjects = new List<GameObject>();
+    [SerializeField] GameObject placementIndicator;
+    [SerializeField] float offset;
+
+    private ARRaycastManager aRRaycastManager;
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    private bool objectPlacedOnce = false;
+    private bool placementPoseIsValid = false;
+    private Pose placementPose;
+
+	void Start()
+	{
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (!objectPlacedOnce)
+        {
+            UpdatePlacementPose();
+            UpdatePlacementIndicator();
+            if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                PlaceObject();
+                objectPlacedOnce = true;
+                placementIndicator.SetActive(false);
+            }
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        aRRaycastManager.Raycast(screenCenter, s_Hits, TrackableType.Planes);
+
+        placementPoseIsValid = s_Hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = s_Hits[0].pose;
+            var cameraForward = Camera.main.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void PlaceObject()
+    {
+        int numObjects = followingObjects.Count;
+        for(int i =0; i<numObjects; i++)
+        {
+            Vector3 position = new Vector3(placementPose.position.x + offset * i, placementPose.position.y, placementPose.position.z);
+            Instantiate(followingObjects[i], position, placementPose.rotation);
+        }
+    }
+   
+}
+
